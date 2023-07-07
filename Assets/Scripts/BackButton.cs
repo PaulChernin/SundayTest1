80bc7cffb5807f63445a1e2ea3eb1d3678aceb10
PaulChernin <pavel@chernin.ru>
2023-07-07 20:20:02 +0300

    android button support

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    public string SceneName;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
